ca7ec3185f61e544262ec7b74102af99eab0026b
RashawnKH <rashawn1999@hotmail.com>
2020-12-01 21:48:53 +0000

    Added Starter class which will hold the method calls to ArrayExercises.java, which will be run in Main.java

---
+++ Starter.java
@@ -0,0 +1,17 @@
+package com.spartaglobal.rashawn;
+
+public class Starter {
+
+
+    public static void start(){
+        ArrayExercises.swap(1,2);
+        ArrayExercises.sumOddOrEvenNumbers();
+        ArrayExercises.switchCase(1);
+        ArrayExercises.fibonacci(8);
+
+
+    }
+
+
+
+}
